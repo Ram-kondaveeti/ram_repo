b92440f30469c40b236fcf5670814e0973538904
Ram-kondaveeti <120016747+Ram-kondaveeti@users.noreply.github.com>
2022-12-08 12:51:02 +0530

    Add files via upload

--- repositary.py
+++ repositary.py
@@ -6,3 +6,20 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+
+
+class Person:
+    def __init__(self, name, sex, profession):
+        # data members (instance variables)
+        self.name = name
+        self.sex = sex
+        self.profession = profession
+
+    # Behavior (instance methods)
+    def show(self):
+        print('Name:', self.name, 'Sex:', self.sex, 'Profession:', self.profession)
+
+    # Behavior (instance methods)
+    def work(self):
+        print(self.name, 'working as a', self.profession)
